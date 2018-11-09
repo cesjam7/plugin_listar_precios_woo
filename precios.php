02d563e348333141857c3cda101a2d5f12a57d0a
Cesar Aquino Maximiliano <cesar7492@gmail.com>
2018-11-09 10:11:46 -0500

    first commit

--- precios.php
+++ precios.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Butrich - Revisar Precios
-Description: Módulo para revisar rápidamente los precios
+Description: Módulo para revisar rápidamente los precios.
 Version: 1.0
 Author: César Aquino Maximiliano
 Author URI: http://cesar.pe
